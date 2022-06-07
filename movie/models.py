3f5b6650cdb128113bd827b1c600f46817fd55e3
GyeongSuKim <rla04722@naver.com>
2022-06-07 21:56:27 +0900

    Feat : 영화정보 크롤링

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class MovieModel(models.Model):
+	class Meta:
+		db_table = "movie"
+
+	from tmdbv3api import TMDb, Movie
+
+	tmdb = TMDb()
+	tmdb.api_key = '0462420f053eaee646c334e3a226605b'
+
+	tmdb.language = 'ko'
+	tmdb.debug = True
+
+	movie = Movie()
+	for i in range(1, 2):
+		popular = movie.popular(i)
+		img = 'https://www.themoviedb.org/t/p/w220_and_h330_face'
+
+		for p in popular:
+			# print(len(p))
+			print('ID : ', p.id)
+			print('제목 : ', p.title)
+			print('줄거리 : ', p.overview)
+			print('사진 url : ', img + p.poster_path)
+			print('평점 : ', round(p.vote_average / 2, 1))
+			print('장르 : ', p.genre_ids)
+			print(' ')
