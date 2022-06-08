fc1199ce9954e46953ba6bfb8483464a2d3fea0a
juhyun1011 <juhyun4567@gmail.com>
2022-06-08 21:38:38 +0900

    Update : admin, models.py

--- post/models.py
+++ post/models.py
@@ -14,5 +14,3 @@ class PostModel(models.Model):
     content = models.TextField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.title
