1611ed2d5df628fb296a3af4db6b722551a02ebd
juhyun1011 <juhyun4567@gmail.com>
2022-06-10 23:05:59 +0900

    Fix : app_name delete

--- post/urls.py
+++ post/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name = 'post'
 urlpatterns = [
     path('detail/post/<int:id>/', views.show_post, name='show-post'),
     path('mypage/', views.show_list, name='show-list'),
